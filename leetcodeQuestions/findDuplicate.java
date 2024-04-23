af5ee0ea65060ab082b31cac3ade33f59607dc21
cratesium <cratesium@gmail.com>
2024-04-23 22:24:11 +0530

    adding leetcode

--- leetcodeQuestions/findDuplicate.java
+++ leetcodeQuestions/findDuplicate.java
@@ -13,3 +13,23 @@ class Solution {
         return toret;
     }
 }
+class Solution {
+    public int findDuplicate(int[] nums) {
+     int toret=0;
+       Map<Integer,Integer>mp= new HashMap<>();
+       for( int i=0;i<nums.length;i++){
+        if(mp.get(nums[i])==null){
+            mp.put(nums[i],1);
+        }else{
+            mp.put(nums[i],mp.get(nums[i])+1);
+        }
+       }
+       for( var v : mp.entrySet()){
+        if(v.getValue()>1){
+            toret=v.getKey();
+            break;
+        }
+       }
+       return toret;
+    }
+}
